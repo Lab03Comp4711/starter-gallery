cbd7e77b8125605d7429968461d778b0f72b2037
ABC <ABC@192.168.1.72>
2017-09-22 21:31:53 -0700

    Added database configuration for our models Added Images model Updated the Gallery and Welcome controller to work with the database Updated changelog
    Signed-off-by:ABC <ABC@192.168.1.72>

---
+++ application/views/_cell.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ *  <td class="oneimage">
+        <a href="./data/Katmai_Crater_1980.jpg" 
+ *      data-lightbox="gallery" data-title="Katmai Crater - Mount Katmai, Alaska ... Posted 2014.05.05 by donald, in landscape"><img src="./data/thumb/Katmai_Crater_1980.jpg"/></a>
+        </td>
+ */
+?>
+
+<a href="./data/{filename}" data-lightbox="gallery"
+   data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
+   <img src="./data/thumb/{filename}"/>
+</a>
